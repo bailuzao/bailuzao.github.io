4f3b1567b7850fe0423b0e7385547532627d464a
bailuzao <2201244738@qq.com>
2025-11-24 23:35:04 +0800

    Site updated: 2025-11-24 23:35:03

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/11/15/使用wireshark对qq发送的图片进行抓包并还原/","2025/11/22/紧急学习报告1/"];function toRandomPost(){
+var posts=["2025/11/15/使用wireshark对qq发送的图片进行抓包并还原/","2025/11/22/紧急学习报告1/","2025/11/24/VMware中虚拟机不同VMnet的区别/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
